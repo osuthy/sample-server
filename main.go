47bb054cfdd6d976265bfcf2fcbd6e84b22d8e46
ShoichiroKitano <ohmybigcatkotarou@gmail.com>
2019-09-20 21:36:01 +0900

    middlewareを導入

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.GET("/", hello)
 	e.Start("0.0.0.0:80")
 }
