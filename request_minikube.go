cdace59ad02ed5aae4a845eda441cc70c29c2bfe
Yuki Kakegawa <fasxyuqi@gmail.com>
2019-10-04 20:09:01 +0900

    goroutineを使って複数namespaceのサーバーにリクエストを送る

--- request_minikube.go
+++ request_minikube.go
@@ -6,29 +6,22 @@ import(
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func main() {
-	// namespcaeの数分、channelを作成する
 
 	namespaces := []string{"hoge", "fuga"}
 	minikube_ip := os.Getenv("minikube_ip")
 	connectionMaps := make(map[string](chan string))
+	wg := &sync.WaitGroup{}
 
 	for _, ns := range namespaces {
 		connectionMaps[ns] = make(chan string)
 	}
 
 	for ns, channel := range connectionMaps {
-			// nodePort, _ := exec.Command(
-			// 	"kubectl", "get", "svc", "-n", ns,
-			// 	"--output=jsonpath='{.items[0].spec.ports[0].nodePort}'").Output()
-			// port, _ := strconv.Atoi(strings.Trim(string(nodePort), "'"))
-			// addr := fmt.Sprintf("%s:%d", minikube_ip, port)
-			// out, _ := exec.Command("curl", addr).Output()
-			// fmt.Print(string(out))
-
-
+		wg.Add(2)
 		go func(ch chan string, ns string) {
 			nodePort, _ := exec.Command(
 				"kubectl", "get", "svc", "-n", ns,
@@ -37,21 +30,13 @@ func main() {
 			addr := fmt.Sprintf("%s:%d", minikube_ip, port)
 			out, _ := exec.Command("curl", addr).Output()
 			ch <- string(out)
+			wg.Done()
 		}(channel, ns)
 
-		go func(ch chan string) {
+		go func(ch chan string, ns string) {
 			fmt.Println(<-ch)
-		}(channel)
+			wg.Done()
+		}(channel, ns)
 	}
-	// minikube_ip := os.Getenv("minikube_ip")
-	// nodePort, _ := exec.Command(
-	// 	"kubectl", "get", "svc", "-n", "micro-test-ns",
-	// 	"--output=jsonpath='{.items[0].spec.ports[0].nodePort}'",
-	// 	).Output()
-
-	// port, _ := strconv.Atoi(strings.Trim(string(nodePort), "'"))
-	// addr := fmt.Sprintf("%s:%d", minikube_ip, port)
-	
-	// out, _ := exec.Command("curl", addr).Output()
-	// fmt.Print(string(out))
+	wg.Wait()
 }
