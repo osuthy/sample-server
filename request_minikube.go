2a2ff7b81f0f679d20053caf6efbbfd083505474
Yuki Kakegawa <fasxyuqi@gmail.com>
2019-10-04 20:16:52 +0900

    リファクタリング

--- request_minikube.go
+++ request_minikube.go
@@ -1,8 +1,8 @@
 package main
 
-import(
-	"os"
+import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -13,30 +13,20 @@ func main() {
 
 	namespaces := []string{"hoge", "fuga"}
 	minikube_ip := os.Getenv("minikube_ip")
-	connectionMaps := make(map[string](chan string))
 	wg := &sync.WaitGroup{}
 
 	for _, ns := range namespaces {
-		connectionMaps[ns] = make(chan string)
-	}
-
-	for ns, channel := range connectionMaps {
-		wg.Add(2)
-		go func(ch chan string, ns string) {
+		wg.Add(1)
+		go func(ns string) {
 			nodePort, _ := exec.Command(
 				"kubectl", "get", "svc", "-n", ns,
 				"--output=jsonpath='{.items[0].spec.ports[0].nodePort}'").Output()
 			port, _ := strconv.Atoi(strings.Trim(string(nodePort), "'"))
 			addr := fmt.Sprintf("%s:%d", minikube_ip, port)
 			out, _ := exec.Command("curl", addr).Output()
-			ch <- string(out)
+			fmt.Print(string(out))
 			wg.Done()
-		}(channel, ns)
-
-		go func(ch chan string, ns string) {
-			fmt.Println(<-ch)
-			wg.Done()
-		}(channel, ns)
+		}(ns)
+		wg.Wait()
 	}
-	wg.Wait()
 }
